eb6eda0b32b5999189e6bb96dc5d5c85191ffe01
Sahil Gupta <66416393+Sahilgupta8423@users.noreply.github.com>
2022-06-01 00:23:18 +0530

    Update settings.py

--- Thefooddude/Thefooddude/Thefooddude/settings.py
+++ Thefooddude/Thefooddude/Thefooddude/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL= "/static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/Thefooddude')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/Thefooddude')
+]
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
