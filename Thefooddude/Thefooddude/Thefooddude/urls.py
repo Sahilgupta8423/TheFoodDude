27bea6a480aa7331297cdf813e55f8fe91bbaaaa
Sahil Gupta <66416393+Sahilgupta8423@users.noreply.github.com>
2022-05-31 17:58:27 +0530

    Update urls.py

--- Thefooddude/Thefooddude/Thefooddude/urls.py
+++ Thefooddude/Thefooddude/Thefooddude/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path("register/", views.register, name ="register"),
     path("login/", views.login, name ="login"),
     path("logout/", views.logout, name ="logout"),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
