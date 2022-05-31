c660ba18cc86b7b31c44c51ab22dc66b41c57527
Sahil Gupta <66416393+Sahilgupta8423@users.noreply.github.com>
2022-06-01 00:21:04 +0530

    Update settings.py

--- Thefooddude/Thefooddude/Thefooddude/settings.py
+++ Thefooddude/Thefooddude/Thefooddude/settings.py
@@ -125,4 +125,4 @@ STATIC_URL= "/static/"
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
